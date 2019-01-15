d9952ff2ab2584532638ee97c208025f5a53795b
ofir <gennin@gmail.com>
2019-01-15 18:11:45 +0200

    initial commit

---
+++ Check-Mate-Challenge/Check-Mate-Challenge/Check-Mate-Challenge.Android/BluetoothDeviceReceiver.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Bluetooth;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Check_Mate_Challenge.Droid
+{
+    public class BluetoothDeviceReceiver : BroadcastReceiver
+    {
+        public static BluetoothAdapter Adapter => BluetoothAdapter.DefaultAdapter;
+
+        public override void OnReceive(Context context, Intent intent)
+        {
+            var action = intent.Action;
+
+            // Found a device
+            switch (action)
+            {
+                case BluetoothDevice.ActionFound:
+                    // Get the device
+                    var device = (BluetoothDevice)intent.GetParcelableExtra(BluetoothDevice.ExtraDevice);
+
+                    // Only update the adapter with items which are not bonded
+                    if (device.BondState != Bond.Bonded)
+                    {
+                      
+                    }
+
+                    break;
+                case BluetoothAdapter.ActionDiscoveryStarted:
+                    Console.WriteLine("Discovery Started...");
+                    break;
+                case BluetoothAdapter.ActionDiscoveryFinished:
+                    Console.WriteLine("Discovery Finished.");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Check-Mate-Challenge/Check-Mate-Challenge/Check-Mate-Challenge/IBth.cs
@@ -0,0 +1,19 @@
+﻿using Check_Mate_Challenge.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Check_Mate_Challenge
+{
+    public interface IBth
+    {
+        void StartScanning();
+        void RegisterBluetoothReceiver();
+        void CancelScanning();
+        void UnregisterBluetoothReceiver();
+        ObservableCollection<Bluetooth> getBondedDevices();
+
+        ObservableCollection<Bluetooth> getNearByDevices();
+    }
+}
